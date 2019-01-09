b52100eaffaefe1efe36214d06906de063c5b0d2
christopher.tero Tero <christopher.tero@gmail.com>
2019-01-09 03:25:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,3 +21,13 @@ function destructivelyRemoveFirstKitten() {
 function appendKitten(name) {
   return [...kittens, name];
 }
+
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function 
